7fd77456137ab73f0d9c6b415f2db0a1df639590
Lim-Byeong-Kuk <bye0414@naver.com>
2024-03-29 15:16:24 +0900

    게시판 페이징 구현

--- src/main/java/com/example/boardrepeat/controller/ArticleController.java
+++ src/main/java/com/example/boardrepeat/controller/ArticleController.java
@@ -1,11 +1,21 @@
 package com.example.boardrepeat.controller;
 
-import com.example.boardrepeat.domain.Article;
+import com.example.boardrepeat.domain.type.SearchType;
+import com.example.boardrepeat.dto.response.ArticleResponse;
+import com.example.boardrepeat.dto.response.ArticleWithCommentsResponse;
+import com.example.boardrepeat.service.ArticleService;
+import com.example.boardrepeat.service.PaginationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,20 +26,35 @@ import java.util.List;
  * /articles/search
  * /articles/search-hashtag
  */
+@RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
+    private final ArticleService articleService;
+    private final PaginationService paginationService;
+
     @GetMapping
-    public String articles(ModelMap map) {
-        map.addAttribute("articles", List.of());
+    public String articles(
+            @RequestParam(required = false) SearchType searchType,
+            @RequestParam(required = false) String searchValue,
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
+            ModelMap map
+    ) {
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> paginationBarNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", paginationBarNumbers);
+
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
-        map.addAttribute("article", "article");
-        map.addAttribute("articleComments", List.of());
+        ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticle(articleId));
+        map.addAttribute("article", article);
+        map.addAttribute("articleComments", article.articleCommentsResponses());
         return "articles/detail";
     }
 }
--- src/test/java/com/example/boardrepeat/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardrepeat/controller/ArticleControllerTest.java
@@ -1,16 +1,29 @@
 package com.example.boardrepeat.controller;
 
 import com.example.boardrepeat.config.SecurityConfig;
+import com.example.boardrepeat.dto.ArticleWithCommentsDto;
+import com.example.boardrepeat.dto.UserAccountDto;
+import com.example.boardrepeat.service.ArticleService;
+import com.example.boardrepeat.service.PaginationService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 @DisplayName("View 컨트롤러 - 게시글")
 @Import(SecurityConfig.class)
@@ -18,6 +31,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 public class ArticleControllerTest {
 
     private final MockMvc mvc;
+
+    @MockBean private ArticleService articleService;    // MockBean 은 @Autowired 로 생성자 주입 안됨
+    @MockBean private PaginationService paginationService;
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -27,14 +43,22 @@ public class ArticleControllerTest {
     @Test
     void articles() throws Exception {
         // given
+        BDDMockito.given(articleService.searchArticles(
+                ArgumentMatchers.eq(null), ArgumentMatchers.eq(null), ArgumentMatchers.any(Pageable.class)))
+                        .willReturn(Page.empty());
+        BDDMockito.given(paginationService.getPaginationBarNumbers(ArgumentMatchers.anyInt(), ArgumentMatchers.anyInt()))
+                        .willReturn(List.of(0,1,2,3,4));
 
         // when & then
         mvc.perform(MockMvcRequestBuilders.get("/articles"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(MockMvcResultMatchers.view().name("articles/index"))
-                .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
+                .andExpect(MockMvcResultMatchers.model().attributeExists("articles"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("paginationBarNumbers"));
 
+        BDDMockito.then(articleService).should().searchArticles(ArgumentMatchers.eq(null), ArgumentMatchers.eq(null), ArgumentMatchers.any(Pageable.class));
+        BDDMockito.then(paginationService).should().getPaginationBarNumbers(ArgumentMatchers.anyInt(), ArgumentMatchers.anyInt());
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
@@ -42,6 +66,7 @@ public class ArticleControllerTest {
     void given_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         // given
         Long articleId = 1L;
+        BDDMockito.given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
         // when & then
         mvc.perform(MockMvcRequestBuilders.get("/articles/" + articleId))
@@ -51,6 +76,7 @@ public class ArticleControllerTest {
                 .andExpect(MockMvcResultMatchers.model().attributeExists("article"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articleComments"));
 
+        BDDMockito.then(articleService).should().getArticle(articleId);
     }
 
     @Disabled
@@ -68,4 +94,32 @@ public class ArticleControllerTest {
 
     }
 
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "lbk",
+                LocalDateTime.now(),
+                "lbk"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(1L,
+                "lbk",
+                "pw",
+                "lbk@gmail.com",
+                "Forest",
+                "memo",
+                LocalDateTime.now(),
+                "lbk",
+                LocalDateTime.now(),
+                "lbk"
+        );
+    }
 }
